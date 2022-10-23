5f4761df2b40215347859090113cad9169bab8d0
nur00103 <nur1398@mail.ru>
2022-10-23 19:56:43 +0400

    API Gateway,Pre Filter and Post Filter...

--- StudentService/src/main/java/com/example/studentservice/feignClients/AddressFeignClient.java
+++ StudentService/src/main/java/com/example/studentservice/feignClients/AddressFeignClient.java
@@ -7,8 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-//// Without Eureka@FeignClient(url = "${address.service.url}" ,value = "address-feign-client" /* we can use the PATH after url*/)
-@FeignClient(value = "address-service") //With eureka client
+//// Without Eureka
+@FeignClient(url = "${address.service.url}" ,value = "address-feign-client" /* we can use the PATH after url*/)
+//@FeignClient(value = "ADDRESS-SERVICE") //With eureka client
 public interface AddressFeignClient {
 
     @GetMapping("/getById/{id}")
